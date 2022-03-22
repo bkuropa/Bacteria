8df1cbf43d0206c620dfe52d63f5a00cc7d47c49
Bryan Kuropatwa <bkuropa@gmail.com>
2022-03-22 14:58:10 -0400

    3 graph

--- plots.js
+++ plots.js
@@ -107,7 +107,7 @@ function buildCharts(sample) {
       marker: {
         size: sample_values,
         color: otu_ids,
-        colorscale: "plasma"
+        colorscale: "icefire"
       }
     }];
 
@@ -123,5 +123,59 @@ function buildCharts(sample) {
     // 3. Use Plotly to plot the data with the layout.
     Plotly.newPlot("bubble", bubbleData, bubbleLayout, {responsive: true}); 
 
+
+        // 1. Create a variable that filters the metadata array for the object with the desired sample number.
+    var metadataArray = data.metadata.filter(data => data.id == sample);
+    
+    // 2. Create a variable that holds the first sample in the metadata array.
+    var metadataFirst = metadataArray[0];
+
+    // Create variables that hold the otu_ids, otu_labels, and sample_values.
+
+    // 3. Create a variable that holds the washing frequency.
+    var washingFreq = metadataArray[0].wfreq;
+
+    // 4. Create the trace for the gauge chart.
+    var gaugeData = [{
+     domain: { x: [0,1], y: [0,1] },
+     value: washingFreq,
+     title: { text: "<b>Belly Button Washing Frequency</b><br>Scrubs per week"},
+     type: "indicator",
+     mode: "guage+number",
+     guage: {
+       axis: {
+         range: [null, 10],
+         tickmode: "array",
+         tickvals: [0,2,4,6,8,10],
+         ticktext: [0,2,4,6,8,10]
+       },
+       bar: {color: "black"},
+       steps: [
+         { range: [0,2], color: "red" },
+         {range: [2,4], color: "orange"},
+         {range: [4,6], color: "yellow"},
+         {range: [6,8], color: "lime"},
+         {range: [8,10], color: "green"}]
+        }
+     }
+    ];
+    
+    // 5. Create the layout for the gauge chart.
+    var gaugeLayout = { 
+     autosize: true,
+     annotations: [{
+       xref: 'paper',
+       yref: 'paper',
+       x: 0.5,
+       xanchor: 'center',
+       y: 0,
+       yanchor: 'center',
+       text: "Gauge displaying belly button washing frequency - weekly",
+       showarrow: false
+     }]
+    };
+
+    // 6. Use Plotly to plot the gauge data and layout.
+    Plotly.newPlot("gauge", gaugeData, gaugeLayout, {responsive: true});
   });
 }
